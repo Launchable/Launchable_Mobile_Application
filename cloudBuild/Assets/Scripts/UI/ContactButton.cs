34023674cc06df875dfeb2914a6dd764b8b03dc5
kylaunchable <30441888+kylaunchable@users.noreply.github.com>
2017-09-15 15:27:42 -0400

    updating the contact buttons

---
+++ cloudBuild/Assets/Scripts/UI/ContactButton.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ContactButton : MonoBehaviour
+{
+    public TargetTracker tracker;
+    public int contactButtonIndex = 0;
+
+    public Sprite[] defaultSprites;
+    Image myImage;
+
+    private void Start()
+    {
+        myImage = this.GetComponent<Image>();
+    }
+
+    public void SetButtonType(int index)
+    {
+        contactButtonIndex = index;
+        myImage.sprite = defaultSprites[contactButtonIndex];
+    }
+
+    public void OpenContact()
+    {
+        switch (contactButtonIndex)
+        {
+            case 0:
+                Application.OpenURL("mailto:" + tracker.GetTargetEmail());
+                break;
+            case 1:
+                Application.OpenURL("tel://" + tracker.GetTargetPhone());
+                break;
+            case 2:
+                Application.OpenURL(tracker.GetTargetWeb());
+                break;
+        }
+    }
+}
